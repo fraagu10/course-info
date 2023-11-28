7c2d59591fd8d5f9fa04210d6858e6c36f11fba1
fraagu10 <aguilarfranco1092@protonmail.com>
2023-11-28 11:55:26 -0600

    added basic exception handling

--- src/main/java/io/prototype/courseinfo/cli/CourseRetriever.java
+++ src/main/java/io/prototype/courseinfo/cli/CourseRetriever.java
@@ -4,5 +4,23 @@ public class CourseRetriever {
 
     public static void main(String... args) {
         System.out.println("Course Retriever initiated...");
+
+        if(args.length == 0) {
+            System.out.println("Please provide an author name as first argument.");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            System.out.println("Unexpected Error.");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private static void retrieveCourses(String authorName) {
+        System.out.println("Retrieving course for author " + authorName);
     }
 }
