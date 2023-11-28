5406f786b572595afbce10710bd1e87ba03d1fa6
fraagu10 <aguilarfranco1092@protonmail.com>
2023-11-28 11:33:48 -0600

    initial commit

---
+++ src/main/java/io/prototype/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,8 @@
+package io.prototype.courseinfo.cli;
+
+public class CourseRetriever {
+
+    public static void main(String... args) {
+        System.out.println("Course Retriever initiated...");
+    }
+}
